fbcb5a963ffdbefe53f3f63931faacb65d351bd3
unknown <beganoo@abv.bg>
2021-08-20 00:00:22 +0300

    first commit

---
+++ dnd1js.js
@@ -0,0 +1,42 @@
+function changePic1()
+{
+  document.getElementById("ll1").src="pics/alt2.png";
+}
+
+function changePic2()
+{
+  document.getElementById("ll1").src="pics/logo.png";
+}
+
+function changePic3()
+{
+  document.getElementById("roarr").src="pics/roar.gif";
+}
+
+function changePic4()
+{
+  document.getElementById("roarr").src="pics/head.png";
+}
+
+
+function changePic5()
+{
+  document.getElementById("ll2").src="pics/alt3.png";
+}
+
+function changePic6()
+{
+  document.getElementById("ll2").src="pics/logo.png";
+}
+
+function changePic7()
+{
+  document.getElementById("ll3").src="pics/alt4.png";
+}
+
+function changePic8()
+{
+  document.getElementById("ll3").src="pics/logo.png";
+}
+
+
